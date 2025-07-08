596249d0736e374155063969fdd2a3ee9305a183
emineaaaa <emine0aydinli3@gmail.com>
2025-07-08 14:43:54 +0300

    vercel hata çozumu 2

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -18,7 +18,7 @@ function ProductList() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch('https://renart-zxl1-nko0zuzi1-emines-projects-6c93615c.vercel.app/api/products');
+        const response = await fetch('https://renart-zxl1.vercel.app/api/products');
         console.log(response);
         if (!response.ok) {
           throw new Error(`HTTP hatası! Durum kodu: ${response.status}`);
